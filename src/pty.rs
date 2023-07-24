fc60f1dadfa8bef9dcb9cf1695e26500ab163ff4
Roland Fredenhagen <dev@modprog.de>
2023-07-24 23:30:21 +0700

    fix: fish disable history based autosuggestions

--- src/pty.rs
+++ src/pty.rs
@@ -171,6 +171,7 @@ impl FishRuntime {
         let config_path = home.join("fish/config.fish");
         let config = "\
 set -U fish_greeting \"\"
+set -U fish_autosuggestion_enabled 0
 function fish_title
 end
 function fish_prompt
