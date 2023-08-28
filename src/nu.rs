9d88283f2b9f956c63ed0c22038837586b6d326e
Ed Page <eopage@gmail.com>
2023-08-28 10:39:52 -0500

    chore: Upgrade to nu to 0.82

--- src/nu.rs
+++ src/nu.rs
@@ -5,7 +5,6 @@ use std::path::PathBuf;
 use std::sync::Arc;
 
 use nu_cli::NuCompleter;
-use nu_command::create_default_context;
 use nu_parser::parse;
 use nu_protocol::{
     engine::{EngineState, Stack, StateWorkingSet},
@@ -184,7 +183,7 @@ fn new_engine(path: &OsStr, home: &Path) -> std::io::Result<(EngineState, Stack)
     let path_len = path.len();
 
     // Create a new engine with default context
-    let mut engine_state = create_default_context();
+    let mut engine_state = EngineState::new();
 
     // New stack
     let mut stack = Stack::new();
