bec20f8461cf74a9d375fe33eca385ea6c829796
Lzu Tao <taolzu@gmail.com>
2024-06-27 23:12:37 +0700

    zsh: bypass security checking for compinit

--- crates/completest-pty/src/lib.rs
+++ crates/completest-pty/src/lib.rs
@@ -74,7 +74,7 @@ impl ZshRuntime {
         let config_path = home.join(".zshenv");
         let config = "\
 fpath=($fpath $ZDOTDIR/zsh)
-autoload -U +X compinit && compinit
+autoload -U +X compinit && compinit -u # bypass compaudit security checking
 precmd_functions=\"\"  # avoid the prompt being overwritten
 PS1='%% '
 PROMPT='%% '
